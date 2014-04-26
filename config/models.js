823d014cd18c74aaf208d5f4a8f00a5beb80bfd8
hayksaakian <fakest.id.ever@gmail.com>
2014-04-26 00:11:32 -0700

    heroky readu?

--- config/models.js
+++ config/models.js
@@ -6,11 +6,16 @@
  * in each of your models.
  */
 
+var connection = 'somePostgresqlServer';
+if(process.env.NODE_ENV === 'development'){
+  connection = 'localDiskDb';
+}
+
 module.exports.models = {
  
   // Your app's default connection.
   // i.e. the name of one of your app's connections (see `config/connections.js`)
   //
   // (defaults to localDiskDb)
-  connection: 'localDiskDb'
+  connection: connection
 };
