e652202dc8f2bec86520824bce847d6b6b3e22c0
hayksaakian <fakest.id.ever@gmail.com>
2014-05-04 13:33:21 -0700

    sorting is a pain

--- api/controllers/ArticleController.js
+++ api/controllers/ArticleController.js
@@ -13,7 +13,7 @@ module.exports = {
 
   index: function (req, res) {
     // console.log(isAuthenticated)
-    Article.find().exec(function foundArticle(err, articles){
+    Article.find().sort('id DESC').exec(function (err, articles){
       if (err) return next(err);
       // console.log(articles)
       res.view('article/index', {
