f334c87116541b81bf350af54d2aaa3857eddff1
hayksaakian <fakest.id.ever@gmail.com>
2014-05-17 16:13:49 -0700

    fixed deleting pages

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -7,14 +7,19 @@
 
 module.exports = {
 	create: function (req, res) {
+    console.log('creating comment')
     Comment.create(req.params.all(), function (err, comment) {
       if (err) return next(err);
+      console.log('done creating comment')
       console.log(comment)
-      res.redirect('/article/'+comment.article.id+'/'+comment.article.clean_title);      
+      var s = '/article/'+comment.article.id+'/'+comment.article.clean_title;
+      res.redirect(s);
     })
   },
   destroy: function(req, res) {
+    console.log('destroying comment')
     Comment.findOne(req.param('id'), function (err, comment) {
+      if(err) return next(err);
       article_id = comment.article.id
       article_title = comment.article.clean_title
       comment.destroy(function (err) {
