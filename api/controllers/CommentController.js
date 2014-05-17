e9582780b2beedf32d4416318c677b737e228db1
hayksaakian <fakest.id.ever@gmail.com>
2014-05-17 16:43:51 -0700

    trying to fix comments

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -8,18 +8,23 @@
 module.exports = {
 	create: function (req, res) {
     console.log('creating comment')
-    Comment.create(req.params.all(), function (err, comment) {
+    Comment.create(req.params.all()).exec(function (err, comment) {
       if (err) return next(err);
       console.log('done creating comment')
       console.log(comment)
-      var s = '/article/'+comment.article.id+'/'+comment.article.clean_title;
-      res.redirect(s);
+      Article.find(comment.article).exec(function (err, article) {
+        if (err) return next(err);
+        var s = '/article/'+article.id+'/'+article.clean_title;
+        console.log(s)
+        res.redirect(s);
+      })
     })
   },
   destroy: function(req, res) {
     console.log('destroying comment')
-    Comment.findOne(req.param('id'), function (err, comment) {
+    Comment.findOne(req.param('id')).populate('article').exec(function (err, comment) {
       if(err) return next(err);
+      console.log('destroying')
       article_id = comment.article.id
       article_title = comment.article.clean_title
       comment.destroy(function (err) {
