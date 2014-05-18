5be57f8de12fce7c25950ddab6719d29528d1050
hayksaakian <fakest.id.ever@gmail.com>
2014-05-18 08:59:21 -0700

    clean up session

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -18,6 +18,7 @@ module.exports = {
   destroy: function (req, res, next) {
     req.session.authenticated = false;
     req.session.user = null;
+    req.session.destroy();
     res.redirect('/');
   },
   create: function(req, res, next){
