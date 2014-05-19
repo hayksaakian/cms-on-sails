1b4ba1ca5516e44068d08285e29162fd271db436
hayksaakian <fakest.id.ever@gmail.com>
2014-05-18 20:33:27 -0700

    robo tarp to block spam in comments

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -6,21 +6,40 @@
  */
 
 module.exports = {
-	create: function (req, res) {
+	create: function (req, res, next) {
+    badguy = false;
     console.log('creating comment')
-    Comment.create(req.params.all()).exec(function (err, comment) {
-      if (err) return next(err);
-      console.log('done creating comment')
-      console.log(comment)
-      console.log(comment.article);
-      Article.findOne({id: comment.article}).exec(function (err, article) {
-        if (err) return next(err);
-        console.log(article);
-        var s = '/article/'+article.id+'/'+article.clean_title;
-        console.log(s)
-        res.redirect(s);
-      })
+    // block bots
+    if(req.param('address') !== 'http://'){
+      console.log('we got a bot by the address')
+      badguy = true
+      return res.badRequest();
+    }
+    tarp_fields = ['contact', 'message']
+    _.each(tarp_fields, function (tarp) {
+      if(req.param(tarp).length > 0){
+        console.log('we got a bot')
+        badguy = true;
+        return res.badRequest();
+      }
     })
+    //
+    if(!badguy){
+      console.log('all clear')
+      Comment.create(req.params.all()).exec(function (err, comment) {
+        if (err) return next(err);
+        // console.log('done creating comment')
+        // console.log(comment)
+        // console.log(comment.article);
+        Article.findOne({id: comment.article}).exec(function (err, article) {
+          if (err) return next(err);
+          // console.log(article);
+          var s = '/article/'+article.id+'/'+article.clean_title;
+          // console.log(s)
+          res.redirect(s);
+        })
+      })
+    }
   },
   destroy: function(req, res) {
     console.log('destroying comment')
