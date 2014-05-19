84a7459a2ca3399b9234eb672db54a9aa34c4d44
hayksaakian <fakest.id.ever@gmail.com>
2014-05-19 12:35:39 -0700

    debug console

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -65,6 +65,7 @@ module.exports = {
 
   'new': function (req, res, next) {
     // console.log(req.params.all())
+    console.log('inside user/new')
     User.findOne({})
     .then(function (user) {
       if(user){
